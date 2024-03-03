a270858857891039834f00595b92e72e910af127
LuciZR <151607296+LuciZR@users.noreply.github.com>
2024-03-03 15:03:06 +0530

    𝙼𝙸𝙰 𝙼𝙳 ⚠︎

--- config.js
+++ config.js
@@ -7,16 +7,18 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  ['918293838182', '𝑳𝒖𝒄𝒊𝒁𝑹🤖', true], 
+  ['916296851422', '𝑳𝒖𝒄𝒊𝐙𝐑👾', true],
+  ['919733377065', '𝑺𝑨𝑹𝑼𑁍︎', true], 
+  ['916297511083', '𝑳𝒖𝒄𝒊𝒁𝑹⚡', true],
+  
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['918293838182', '916296851422', '919733377065','916297511083'] 
+global.prems = ['918293838182', '916296851422', '919733377065','916297511083']
+global.allowed = ['918293838182', '916296851422', '919733377065','916297511083']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -24,6 +26,7 @@ global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())]
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
+global.beta = 'mLxstUwm'
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -50,29 +53,29 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝑴𝑰𝑨 𝑴𝑫'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
+global.packname = '𝑳𝒖𝒄𝒊𝒁𝑹' 
+global.author = '@LuciZR' 
+global.menuvid = 'https://i.imgur.com/eqkBKsZ.mp4'
+global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029Va9S1ZW17En4Yh6hz63R\n' 
+global.dygp = 'https://chat.whatsapp.com/HuQXSSgC80hERPfi4D4D2i'
+global.fgsc = 'https://github.com/LuciZR/Mia-Md' 
+global.fgyt = 'https://youtube.com/@I.AMLUCI'
+global.fgpyp = 'https://youtube.com/@I.AMLUCI'
+global.fglog = 'MIA.jpg' 
+global.thumb = fs.readFileSync('./MIA.jpg')
 
 
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*🕣 _𝑴𝑰𝑨 𝑰𝑺 𝑳𝑶𝑫𝑰𝑵𝑮....Ꙭ_*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '⌛'
 global.dmoji = '🤭'
-global.done = '✅'
+global.done = '✔️'
 global.error = '❌' 
 global.xmoji = '🔥' 
 
 global.multiplier = 69 
-global.maxwarn = '3' 
+global.maxwarn = '3' // máxima advertencias
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
